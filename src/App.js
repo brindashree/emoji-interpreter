aae794d8a8aa9949750d4adc408ca453810d6aa7
Brindashree C B <brindashree@users.noreply.github.com>
2020-12-19 10:17:52 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDatabase = {
+  "😀": "Grinning Face",
+  "😃": "Grinning Face with Big Eyes",
+  "😄": "Grinning Face with Smiling Eyes",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😆": "Grinning Squinting Face",
+  "😅": "Grinning Face with Sweat",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "🙃": "Upside-Down Face",
+  "😉": "Winking Face",
+  "😊": "Smiling Face with Smiling Eyes",
+  "😇": "Smiling Face with Halo",
+  "🥰": "Smiling Face with Hearts",
+  "😍": "Smiling Face with Heart-Eyes",
+  "🤩": "Star-Struck",
+  "😘": "Face Blowing a Kiss",
+  "😗": "Kissing Face",
+  "😚": "Kissing Face with Closed Eyes",
+  "😙": "Kissing Face with Smiling Eyes",
+  "😋": "Face Savoring Food",
+  "😛": "Face with Tongue",
+  "😜": "Winking Face with Tongue",
+  "🤪": "Zany Face",
+  "😝": "Squinting Face with Tongue",
+  "🤑": "Money-Mouth Face",
+  "🤗": "Hugging Face",
+  "🤭": "Face with Hand Over Mouth",
+  "🤫": "Shushing Face",
+  "🤔": "Thinking Face",
+  "🤐": "Zipper-Mouth Face",
+  "🤨": "Face with Raised Eyebrow",
+  "😐": "Neutral Face",
+  "😑": "Expressionless Face",
+  "😶": "Face Without Mouth",
+  "😏": "Smirking Face",
+  "😒": "Unamused Face",
+  "🙄": "Face with Rolling Eyes",
+  "😬": "Grimacing Face",
+  "🤥": "Lying Face",
+  "😌": "Relieved Face",
+  "😔": "Pensive Face",
+  "😪": "Sleepy Face",
+  "🤤": "Sleeping Face",
+  "😷": "Face with Medical Mask",
+  "🤒": "Face with Thermometer",
+  "🤕": "Face with Head-Bandage",
+  "🤢": "Nauseated Face",
+  "🤮": "Face Vomiting",
+  "🤧": "Sneezing Face",
+  "🥵": "Hot Face",
+  "🥶": "Cold Face",
+  "🥴": "Woozy Face",
+  "😵": "Dizzy Face",
+  "🤯": "Exploding Head",
+  "🤠": "Cowboy Hat Face",
+  "🥳": "Partying Face",
+  "😎": "Smiling Face with Sunglasses",
+  "🤓": "Nerd Face",
+  "🧐": "Face with Monocle",
+  "😕": "Confused Face",
+  "😟": "Worried Face",
+  "🙁": "Slightly Frowning Face",
+  "😮": "Face with Open Mouth",
+  "😯": "Hushed Face",
+  "😲": "Astonished Face",
+  "😳": "Flushed Face",
+  "🥺": "Pleading Face",
+  "😦": "Frowning Face with Open Mouth",
+  "😧": "Anguished Face",
+  "😨": "Fearful Face",
+  "😰": "Anxious Face with Sweat",
+  "😥": "Sad but Relieved Face",
+  "😢": "Crying Face",
+  "😭": "Loudly Crying Face",
+  "😱": "Face Screaming in Fear",
+  "😖": "Confounded Face",
+  "😣": "Persevering Face",
+  "😞": "Disappointed Face",
+  "😓": "Downcast Face with Sweat",
+  "😩": "Weary Face",
+  "😫": "Tired Face",
+  "🥱": "Yawning Face",
+  "😤": "Face with Steam From Nose",
+  "😡": "Pouting Face",
+  "😠": "Angry Face",
+  "🤬": "Face with Symbols on Mouth",
+  "😈": "Smiling Face with Horns",
+  "👿": "Angry Face with Horns",
+  "💀": "Skull",
+  "☠️": "Skull and Crossbones",
+  "💩": "Pile of Poo",
+  "🤡": "Clown Face",
+  "👹": "Ogre",
+  "👺": "Goblin",
+  "👻": "Ghost"
+};
+
+var emojiInDb = Object.keys(emojiDatabase);
+
+export default function App() {
+  var [output, setOutut] = useState("");
+
+  function emojiHandler(event) {
+    var userInput = event.target.value;
+    var output = emojiDatabase[userInput];
+    if (output === undefined) {
+      output = "we don't have that emoji in the database";
+    }
+    setOutut(output);
+  }
+
+  function emojiClick(emoji) {
+    var output = emojiDatabase[emoji];
+    setOutut(output);
+  }
+
+  return (
+    <div className="App">
+      <h1 id="header">Emoji Interpreter</h1>
+      <p id="tagline">Express yourself better</p>
+      <div className="container">
+        <input
+          id="input"
+          onChange={emojiHandler}
+          placeholder="paste your emoji here"
+        ></input>
+        <p id="meaning">Meaning 👇</p>
+        <center>
+          <div id="output">{output}</div>
+        </center>
+
+        {emojiInDb.map(function flagmap(emoji) {
+          return (
+            <span
+              id="emoji"
+              onClick={() => emojiClick(emoji)}
+              key={emoji}
+              style={{ cursor: "pointer" }}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
+      <div id="footer">By Brindashree C B</div>
+    </div>
+  );
+}
